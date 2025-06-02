789b246f4bb39e4f6dd85af6fb59807dfa3fbb17
sil <silviberat@gmail.com>
2025-06-02 23:53:13 +0200

    refactor: remove console log for user in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { useAuth } from '@/context/AuthContext'
 
 export default function Home() {
   const { user } = useAuth()
-  console.log(user)
 
   return (
     <div className="bg-grey-500">
